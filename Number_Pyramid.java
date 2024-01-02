45530e1df512520b2cbf3cf6e5d5b42ad5fb10fc
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-03 00:18:01 +0530

    Add files via upload

---
+++ Number_Pyramid.java
@@ -0,0 +1,19 @@
+
+public class Number_Pyramid {
+
+	public static void main(String[] args) {
+		int n = 5;
+				//Space
+		for(int a = 1; a<=n;a++ ) {
+			for(int b = 1; b<=n-a; b++) {
+				System.out.print(" ");
+			}
+			for(int b = 1; b<=a; b++) {
+				System.out.print(a+ " ");
+			}
+			System.out.println();
+		}
+		}
+
+	}
+
